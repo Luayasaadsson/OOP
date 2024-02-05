fa2525421693877da16f148e816ed6cd33606410
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-02-05 23:35:41 +0100

    Html, css och js startkod
    Börjar projektet med olika knappar med id:s att kunna ändra bakgrundsfärg med, samt css styling

---
+++ Del-1/klass-color.js
@@ -0,0 +1,9 @@
+// Googlade upp vad en konstruktorfunktion är.
+// Exempel från W3schools
+
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
